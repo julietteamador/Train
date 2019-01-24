a3832165feb1c7b21f5c134ae0ea982e062e3469
Julie <154025@dadeschools.net>
2019-01-24 18:39:53 -0500

    api key

---
+++ Assets/javascript.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyDqM8UeJDtlBsColWfY6EqoP8rvt9bKAxc",
+    authDomain: "train-391bb.firebaseapp.com",
+    databaseURL: "https://train-391bb.firebaseio.com",
+    projectId: "train-391bb",
+    storageBucket: "train-391bb.appspot.com",
+    messagingSenderId: "1079051519374"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Train
+Create Train schedule and form 
 Train Activity Basic 
+Create API key and initialize firebase 
+Create button "Add Train" then update HTML 
+Create a way to calculate train spots 
+Calculate train times 
